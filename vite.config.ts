d24f0da274f03a6216dade082bb4ef81b97f4fbf
Fran Camargo <francamargoads@gmail.com>
2026-05-10 15:34:08 -0300

    netlify edges

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyReactRouter from "@netlify/vite-plugin-react-router"
 
 export default defineConfig({
   base: '/bestElements/',
-  plugins: [react()],
+  plugins: [react(), netlifyReactRouter({edge: true})],
 })
